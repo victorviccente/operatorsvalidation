66e336b4a67f12ff149f90c0d4ee0278859a9b64
Victor Vicente <108288527+victorviccente@users.noreply.github.com>
2023-06-15 11:26:41 -0300

    Add files via upload

---
+++ EqualsJava/Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    int numero1 = 12;
+    int numero2 = 15;
+
+    System.out.println(numero1 == numero2); // false
+
+    System.out.println(numero1 > numero2); // false
+    
+    System.out.println(numero1 < numero2); // true
+
+    System.out.println(numero1 >= numero2); // false
+
+    System.out.println(numero1 <= numero2); // true
+
+    System.out.println(numero1 != numero2); // true
+    
+  }
+}
